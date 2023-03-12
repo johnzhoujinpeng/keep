b247da3f8de28ea1965fcc723da32e7990140bec
johnzhoujinpeng <20g061@suzuka.ac.jp>
2023-03-12 17:05:02 +0900

    test

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+        public static void main(String[] args){
+
+            char grade='c';
+            switch (grade){
+                case 'a':
+                    System.out.println("youxiu");
+                    break;
+                case 'b':
+                    System.out.println("lianghao");
+                    break;
+            case 'c':
+                    System.out.println("putong");
+                break;
+                default:
+                    System.out.println("weizhi");
+
+
+            }
+
+        System.out.println("等级是"+grade);
+
+
+
+    }
+}
