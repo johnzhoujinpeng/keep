14b0d2e46f803f77f073932dc33ffd7d9d0579d0
johnzhoujinpeng <20g061@suzuka.ac.jp>
2023-03-14 18:22:26 +0900

    test

---
+++ code2.java
@@ -0,0 +1,10 @@
+package code;
+
+public class code2 {
+public static void main(String[]args) {
+	System.out.println("HelloWorld"); 
+	
+	
+}
+	
+}
---
+++ code3.java
@@ -0,0 +1,26 @@
+package code;
+
+public class code3 {
+String breed;
+int size;
+String colour;
+int age;
+
+void eat() {
+	
+}
+
+void run() {
+	
+}
+
+void sleep() {
+	
+}
+
+void name() {
+	
+	
+}
+
+}
