00a646401b93eb7880bb92e005fb555e2af33853
johnzhoujinpeng <127651171+johnzhoujinpeng@users.noreply.github.com>
2024-04-23 16:34:25 +0900

    Add files via upload
    efsd d

---
+++ arraylist_stu.java
@@ -0,0 +1,32 @@
+package abc;
+/*
+import java.util.ArrayList;
+
+public class arraylist_stu {
+
+	public static void main(String[]args) {
+		
+		ArrayList<Student> stu=new ArrayList<>();
+		
+		Student stu1=new Student("zhangsan","男",23);
+		Student stu2=new Student("lisis","女",26);
+		Student stu3=new Student("wangwu","男",67);
+		Student stu4=new Student("zhaoliu","女",43);
+		
+		stu.add(stu1);
+		stu.add(stu2);
+		stu.add(stu3);
+		stu.add(stu4);
+		
+		for(int i=0;i<stu.size();i++) {
+			
+		System.out.println(stu.get(i).getName()+","+stu.get(i).getSex()+","+stu.get(i).getAge());	
+		}
+		
+		
+		
+		
+		
+	}*/
+	
+
---
+++ method.java
@@ -0,0 +1,34 @@
+package abc;
+
+public class method {
+	
+	public static void main(String[]args) {
+		
+	int arr[]= {22,33,44,55,66};
+		a(arr);
+		
+		
+	}
+
+
+
+	
+	
+	
+public static void a(int arr[]){
+	
+for
+	(int i=0;i<arr.length;i++) {
+
+}
+	
+	
+}	
+
+}
+
+
+	
+	
+	
+
