044493767c3dfaf98e9c859b79101112e8f7a147
johnzhoujinpeng <127651171+johnzhoujinpeng@users.noreply.github.com>
2024-04-23 16:04:37 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,33 @@
+package abcd;
+
+public class main {
+ public static void main(String[]args) {
+	 
+	 int a=10;
+	 int b=15;
+	 a(a);
+	 
+	 
+ }
+ 
+// ASFSDFS
+ 
+public static  void a(int a){
+	int b=15;
+	 System.out.println(a+b);
+	 b(a);
+	 
+ }
+public static void  b(int a){
+	int b=15;
+	 System.out.println(b);
+	 
+     c(b);
+	
+ }
+ 
+public static void  c(int a){
+	int b=15;
+	 System.out.println(b);
+ }
+}
