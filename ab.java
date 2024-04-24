0100df6a70f0c15d50d65aaf606c2a0f01009049
zhoujinpeng <2907614782@qq.com>
2024-04-24 09:52:22 +0900

    111

---
+++ ab.java
@@ -0,0 +1,43 @@
+package abc;
+import java.util.Scanner; 
+public class ab {
+    public static void main (String[]args) {
+      	Scanner sc=new Scanner(System.in);
+    System.out.println("请输入今天星期几");
+    int a=sc.nextInt();
+   dsd 
+   
+    	switch(a) {
+    	case 1 :
+    		System.out.println("吃肉");
+    		break;
+    	case 2:	
+    		System.out.println("吃饭");
+    		break;
+    	case 3:
+    		System.out.println("去玩");
+    		break;
+    	case 4:
+    		System.out.println("约会");
+    		break;
+    	case 5:
+    		System.out.println("唱歌");
+    		break;
+    	case 6:
+    		System.out.println("跳舞");
+    		break;
+    	case 7:
+    		System.out.println("学习");
+    		break;
+    		default: 
+    			System.out.println("输入错误");
+    }
+    
+      	
+      	
+    	
+    }
+	
+	
+	
+}
